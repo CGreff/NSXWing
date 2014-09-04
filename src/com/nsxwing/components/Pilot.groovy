87ed0630905fda38f2661a2e0ed6ea468d82595f
greffc <greffc@greffc-mbpdu.oa.oclc.org>
2014-09-04 16:55:45 -0400

    Making Ship and Pilot immutable

--- src/com/nsxwing/components/Pilot.groovy
+++ src/com/nsxwing/components/Pilot.groovy
@@ -2,11 +2,13 @@ package com.nsxwing.components
 
 import com.nsxwing.components.abilities.Ability
 import com.nsxwing.components.equipment.EquipmentSlot
+import groovy.transform.Immutable
 
 /**
  * Class describing the Pilot. Includes Shield and Hull values instead of the ship because the YT-1300
  * has variable shields, hull, and attack based on the chosen pilots.
  */
+@Immutable(copyWith = true)
 public class Pilot {
     Ability ability
     Ship ship
--- src/com/nsxwing/components/Ship.groovy
+++ src/com/nsxwing/components/Ship.groovy
@@ -2,10 +2,12 @@ package com.nsxwing.components
 
 import com.nsxwing.components.actions.Action
 import com.nsxwing.movement.Maneuver
+import groovy.transform.Immutable
 
 /**
  * Class describing a Ship and its contained field(s).
  */
+@Immutable(copyWith = true)
 public class Ship {
     Set<Maneuver> maneuvers
     Set<Action> actions
